93af6ac82acecc4476a8004e536436d2a7c8145f
huycv <huycv@neu.edu.vn>
2026-03-13 15:23:35 +0700

    2026.03.13 15:23

--- search.js
+++ search.js
@@ -24,7 +24,7 @@ let usingRemoteEmbed = false;
 let usingOllamaEmbed = false;
 
 const OLLAMA_BASE = (process.env.OLLAMA_BASE_URL || "http://host.docker.internal:11434").replace(/\/$/, "");
-const OLLAMA_EMBED_MODEL = process.env.OLLAMA_EMBEDDING_MODEL || "nomic-embed-text";
+const OLLAMA_EMBED_MODEL = process.env.OLLAMA_EMBEDDING_MODEL || "qwen3-embedding:8b";
 
 export async function initEmbedding() {
   if (embedder || usingRemoteEmbed || usingOllamaEmbed) return true;
